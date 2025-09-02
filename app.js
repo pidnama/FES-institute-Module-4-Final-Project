ac96a4cce61b17c0e3a0844bc128b72420bee47a
Amandip Singh <amandips181@gmail.com>
2025-09-02 13:58:55 +1000

    added search and sort functionlity. Currently implementing loading state

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
-const moviesWrapperEl = document.querySelector(".movies__wrapper");
+// const moviesWrapperEl = document.querySelector(".movies__wrapper");
+// const searchInputEl = document.getElementById("search-input");
+// const searchFormEl = document.getElementById("search-form");
+// const selectElement = document.querySelector("select");
+// let searchItem = "";
 
-async function main() {
-  const moviesResponse = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=24e4873b&s=sinners`);
-  const moivesDataObjects = await moviesResponse.json();
-  const moviesData = moivesDataObjects.Search;
+// searchFormEl.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   searchItem = searchInputEl.value;
+//   main();
+// });
 
-  moviesWrapperEl.innerHTML = moviesData
-    .map((movie) => {
-      return renderMovies(movie);
-    })
-    .slice(0, 6)
-    .join("");
-}
+// async function main(filter) {
+//   if (searchItem === "") {
+//     return (moviesWrapperEl.innerHTML = "search for a movie!");
+//   } else {
+//     const moviesResponse = await fetch(
+//       `https://www.omdbapi.com/?i=tt3896198&apikey=24e4873b&s=${searchItem}`
+//     );
+//     const moivesDataObjects = await moviesResponse.json();
+//     const moviesData = moivesDataObjects.Search;
 
-main();
+//     if (filter === "LOW_TO_HIGH") {
+//       moviesData.sort((a, b) => a.Year - b.Year);
+//       console.log(moviesData);
+//     }
 
-function renderMovies(movie) {
-  return `<div class="movie">
-    <figure class="movie__img--container">
-         <img src="${movie.Poster}" alt="" class="movie___img">
-    </figure>
-    <div class="movie__title">${movie.Title}</div>
-        <div class="movie__release-date">${movie.Year}</div>
-    </div>`;
-}
+//     if (filter === "HIGH_TO_LOW") {
+//       moviesData.sort((a, b) => b.Year - a.Year);
+//       console.groupCollapsed(moviesData);
+//     }
+
+//     if (filter === "A_TO_Z") {
+//       moviesData.sort((a, b) => a.Title.localeCompare(b.Title));
+//     }
+//     moviesWrapperEl.innerHTML = moviesData
+//       .map((movie) => {
+//         return renderMoviesHTML(movie);
+//       })
+//       .slice(0, 6)
+//       .join("");
+//   }
+// }
+
+// function renderMoviesHTML(movie) {
+//   return `<div class="movie">
+//     <figure class="movie__img--container">
+//          <img src="${movie.Poster}" alt="" class="movie___img">
+//     </figure>
+//     <div class="movie__title">${movie.Title}</div>
+//         <div class="movie__release-date">${movie.Year}</div>
+//     </div>`;
+// }
+
+// function filterMovies() {
+//   selectElement.addEventListener("change", (e) => {
+//     main(e.target.value);
+//   });
+// }
+
+// filterMovies();
